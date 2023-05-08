f831d9b7ef042587f7ae14f9ffc57b882ee68c97
Kristina <KYAkovenko@inno.tech>
2023-05-08 19:54:05 +0300

    добавлен расчет суммы, деления, вычитания, умножения и вывод в консоль

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:" );
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int number_1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int number_2 = new Scanner(System.in).nextInt();
+        int sum = number_1 + number_2;
+        int difference = number_1 - number_2;
+        int multiply = number_1 * number_2;
+        double quotient = (double)number_1 / number_2;
+        // практика остатка от деления
+        int remainder = number_1 % number_2;
+        System.out.println("Результат сложения: " + sum);
+        System.out.println("Результат вычитания: " + difference);
+        System.out.println("Результат умножения: " + multiply);
+        System.out.println("Результат деления: " + quotient);
+        // не входит в задание, просто для практики
+        System.out.println("Остаток от деления: " + remainder);
+
+
     }
 }
